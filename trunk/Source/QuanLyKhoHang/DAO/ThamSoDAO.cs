f9ced02c4be6a11cc57e740f4ee9494ae58fecae
damthieuquang@gmail.com <damthieuquang@gmail.com@3525e459-eec0-bede-41f7-e7cad6b7dfdf>
2013-05-25 15:35:44 +0000

    Đã xong các lớp Phiếu Xuất, Phiếu Nhập và Tham Số

--- trunk/Source/QuanLyKhoHang/DAO/ThamSoDAO.cs
+++ trunk/Source/QuanLyKhoHang/DAO/ThamSoDAO.cs
@@ -10,9 +10,69 @@ namespace DAO
 {
     public class ThamSoDAO
     {
+
+        public static bool InsertThamSo(ThamSoDTO thamSoDTO)
+        {
+            List<SqlParameter> sqlParamas = new List<SqlParameter>();
+            sqlParamas.Add(new SqlParameter("@MaThamSo", thamSoDTO.MaThamSo));
+            sqlParamas.Add(new SqlParameter("@TenThamSo", thamSoDTO.TenThamSo));
+            sqlParamas.Add(new SqlParameter("@GiaTri", thamSoDTO.GiaTri));      
+            return DataProvider.ExecuteNoneQuery("usp_InsertThamSo", sqlParamas);
+        }
+
+        public static bool UpdateThamSo(ThamSoDTO thamSoDTO)
+        {
+            List<SqlParameter> sqlParamas = new List<SqlParameter>();
+            sqlParamas.Add(new SqlParameter("@MaThamSo", thamSoDTO.MaThamSo));
+            sqlParamas.Add(new SqlParameter("@TenThamSo", thamSoDTO.TenThamSo));
+            sqlParamas.Add(new SqlParameter("@GiaTri", thamSoDTO.GiaTri)); 
+            return DataProvider.ExecuteNoneQuery("usp_UpdateThamSoById", sqlParamas);
+        }
+
+        public static bool DeleteThamSo(ThamSoDTO thamSoDTO)
+        {
+            List<SqlParameter> sqlParamas = new List<SqlParameter>();
+            sqlParamas.Add(new SqlParameter("@MaThamSo", thamSoDTO.MaThamSo));
+            //SqlParameter para = new SqlParameter("@MaPhieuNhap",phieuNhapDTO.MaPhieuNhap); mot cach viet khac
+            return DataProvider.ExecuteNoneQuery("usp_DeleteThamSoById", sqlParamas);
+        }
+
+        public static List<ThamSoDTO> SelectThamSoAll()
+        {
+            DataTable dataTable = DataProvider.ExecuteReader("usp_SelectThamSoAll");
+            List<ThamSoDTO> listThamSoDTO = new List<ThamSoDTO>();
+            foreach (DataRow dataRow in dataTable.Rows)
+            {
+                ThamSoDTO thamSoDTO = new ThamSoDTO();
+                thamSoDTO.MaThamSo = dataRow["MaThamSo"].ToString();
+                thamSoDTO.TenThamSo = dataRow["TenThamSo"].ToString();
+                thamSoDTO.GiaTri = dataRow["GiaTri"].ToString();
+                listThamSoDTO.Add(thamSoDTO);
+            }
+            return listThamSoDTO;
+        }
+
+        public static List<ThamSoDTO> SelectThamSoById(string MaThamSo)
+        {
+            List<SqlParameter> sqlParamas = new List<SqlParameter>();
+            sqlParamas.Add(new SqlParameter("@MaThamSo", MaThamSo));
+            DataTable dataTable = DataProvider.ExecuteReader("usp_SelectThamSoById");
+            List<ThamSoDTO> listthamSoDTO = new List<ThamSoDTO>();
+            foreach (DataRow dataRow in dataTable.Rows)
+            {
+                ThamSoDTO thamSoDTO = new ThamSoDTO();
+                thamSoDTO.MaThamSo = dataRow["MaThamSo"].ToString();
+                thamSoDTO.TenThamSo = dataRow["TenThamSo"].ToString();
+                thamSoDTO.GiaTri = dataRow["GiaTri"].ToString();
+                listthamSoDTO.Add(thamSoDTO);
+            }
+            return listthamSoDTO;
+        }
+
         public static string CreateThamSoId()
         {
             return (string)DataProvider.ExecuteScalar("usp_CreateThamSoId");
         }
+
     }
 }
