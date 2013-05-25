b1fcd80608a59d76235a075276c4a5f42da3a911
damthieuquang@gmail.com <damthieuquang@gmail.com@3525e459-eec0-bede-41f7-e7cad6b7dfdf>
2013-05-25 15:31:23 +0000

    Đã xong các lớp Phiếu Xuất, Phiếu Nhập và Tham Số

--- Source/QuanLyKhoHang/BUS/PhieuNhapBUS.cs
+++ Source/QuanLyKhoHang/BUS/PhieuNhapBUS.cs
@@ -9,5 +9,29 @@ namespace BUS
 {
     public class PhieuNhapBUS
     {
+        public static List<PhieuNhapDTO> SelectPhieuNhapAll()
+        {
+            return PhieuNhapDAO.SelectPhieuNhapAll();
+        }
+
+        public static List<PhieuNhapDTO> SelectPhieuNhapById(string MaPhieuNhap)
+        {
+            return PhieuNhapDAO.SelectPhieuNhapById(MaPhieuNhap);
+        }
+
+        public static bool InsertPhieuNhap(PhieuNhapDTO phieuNhapDTO)
+        {
+            return PhieuNhapDAO.InsertPhieuNhap(phieuNhapDTO);
+        }
+
+        public static bool UpdatePhieuNhap(PhieuNhapDTO phieuNhapDTO)
+        {
+            return PhieuNhapDAO.UpdatePhieuNhap(phieuNhapDTO);
+        }
+
+        public static bool DeletePhieuNhap(PhieuNhapDTO phieuNhapDTO)
+        {
+            return PhieuNhapDAO.DeletePhieuNhap(phieuNhapDTO);
+        }
     }
 }
--- Source/QuanLyKhoHang/DAO/PhieuNhapDAO.cs
+++ Source/QuanLyKhoHang/DAO/PhieuNhapDAO.cs
@@ -19,6 +19,7 @@ namespace DAO
             sqlParamas.Add(new SqlParameter("@MaNhanVien", phieuNhapDTO.MaNhanVien));
             return DataProvider.ExecuteNoneQuery("usp_InsertPhieuNhap",sqlParamas);
         }
+
         public static bool UpdatePhieuNhap(PhieuNhapDTO phieuNhapDTO)
         {
             List<SqlParameter> sqlParamas = new List<SqlParameter>();
@@ -27,6 +28,48 @@ namespace DAO
             sqlParamas.Add(new SqlParameter("@NgayNhan", phieuNhapDTO.NgayNhan));
             sqlParamas.Add(new SqlParameter("@MaNhanVien", phieuNhapDTO.MaNhanVien));
             return DataProvider.ExecuteNoneQuery("usp_UpdatePhieuNhapById",sqlParamas);
+        }
+
+        public static bool DeletePhieuNhap(PhieuNhapDTO phieuNhapDTO)
+        {
+            List<SqlParameter> sqlParamas = new List<SqlParameter>();
+            sqlParamas.Add(new SqlParameter("@MaPhieuNhap",phieuNhapDTO.MaPhieuNhap));
+            //SqlParameter para = new SqlParameter("@MaPhieuNhap",phieuNhapDTO.MaPhieuNhap); mot cach viet khac
+            return DataProvider.ExecuteNoneQuery("usp_DeletePhieuNhapById", sqlParamas);
+        }
+
+        public static List<PhieuNhapDTO> SelectPhieuNhapAll()
+        {
+            DataTable dataTable = DataProvider.ExecuteReader("usp_SelectPhieuNhapAll");
+            List<PhieuNhapDTO> listPhieuNhapDTO = new List<PhieuNhapDTO>();
+            foreach (DataRow dataRow in dataTable.Rows)
+            {
+                PhieuNhapDTO phieuNhapDTO = new PhieuNhapDTO();
+                phieuNhapDTO.MaPhieuNhap = dataRow["MaPhieuNhap"].ToString();
+                phieuNhapDTO.MaDonHang = dataRow["MaDonHang"].ToString();
+                phieuNhapDTO.NgayNhan = DateTime.Parse(dataRow["NgayNhan"].ToString());
+                phieuNhapDTO.MaNhanVien = dataRow["MaNhanVien"].ToString();
+                listPhieuNhapDTO.Add(phieuNhapDTO);
+            }
+            return listPhieuNhapDTO;
+        }
+
+        public static List<PhieuNhapDTO> SelectPhieuNhapById(string MaPhieuNhap)
+        {
+            List<SqlParameter> sqlParamas = new List<SqlParameter>();
+            sqlParamas.Add(new SqlParameter("@MaPhieuNhap",MaPhieuNhap));
+            DataTable dataTable = DataProvider.ExecuteReader("usp_SelectPhieuNhapById");
+            List<PhieuNhapDTO> listphieuNhapDTO = new List<PhieuNhapDTO>();
+            foreach (DataRow dataRow in dataTable.Rows)
+            {
+                PhieuNhapDTO phieuNhapDTO = new PhieuNhapDTO();
+                phieuNhapDTO.MaPhieuNhap = dataRow["MaPhieuNhap"].ToString();
+                phieuNhapDTO.MaDonHang = dataRow["MaDonHang"].ToString();
+                phieuNhapDTO.NgayNhan = DateTime.Parse(dataRow["NgayNhan"].ToString());
+                phieuNhapDTO.MaNhanVien = dataRow["MaNhanVien"].ToString();
+                listphieuNhapDTO.Add(phieuNhapDTO);
+            }
+            return listphieuNhapDTO;
         }
     }
 }
