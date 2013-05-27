bdea04721deb99dc4562cdfe2ce50cf356e82762
phale20202007@gmail.com <phale20202007@gmail.com@3525e459-eec0-bede-41f7-e7cad6b7dfdf>
2013-05-27 08:05:40 +0000

    Tao ChiTietPhieuXuatBUS, LoaiPhieuXuatBUS,LoaiSanPhamBUS

--- trunk/Source/QuanLyKhoHang/BUS/ChiTietPhieuXuatBUS.cs
+++ trunk/Source/QuanLyKhoHang/BUS/ChiTietPhieuXuatBUS.cs
@@ -9,5 +9,30 @@ namespace BUS
 {
     public class ChiTietPhieuXuatBUS
     {
+        public static bool InsertChiTietPhieuXuat(ChiTietPhieuXuatDTO chiTietPhieuXuatDTO)
+        {
+            return ChiTietPhieuXuatDAO.InsertChiTietPhieuXuat(chiTietPhieuXuatDTO);
+        }
+
+        public static bool UpdateChiTietPhieuXuat(ChiTietPhieuXuatDTO chiTietPhieuXuatDTO)
+        {
+            return ChiTietPhieuXuatDAO.UpdateChiTietPhieuXuat(chiTietPhieuXuatDTO);
+        }
+
+        public static bool DeleteChiTietPhieuXuat(ChiTietPhieuXuatDTO chiTietPhieuXuatDTO)
+        {
+            return ChiTietPhieuXuatDAO.DeleteChiTietPhieuXuat(chiTietPhieuXuatDTO);
+        }
+
+        public static List<ChiTietPhieuXuatDTO> SelectChiTietPhieuXuatAll()
+        {
+            return ChiTietPhieuXuatDAO.SelectChiTietPhieuXuatAll();
+        }
+
+        public static ChiTietPhieuXuatDTO SelectChiTietPhieuXuatById(string MaChiTietPhieuXuat)
+        {
+            return ChiTietPhieuXuatDAO.SelectChiTietPhieuXuatById(MaChiTietPhieuXuat);
+        }
     }
+
 }
--- trunk/Source/QuanLyKhoHang/BUS/LoaiPhieuXuatBUS.cs
+++ trunk/Source/QuanLyKhoHang/BUS/LoaiPhieuXuatBUS.cs
@@ -9,5 +9,29 @@ namespace BUS
 {
     public class LoaiPhieuXuatBUS
     {
+        public static bool InsertLoaiPhieuXuat(LoaiPhieuXuatDTO loaiPhieuXuatDTO)
+        {
+            return LoaiPhieuXuatDAO.InsertLoaiPhieuXuat(loaiPhieuXuatDTO);
+        }
+
+        public static bool UpdateLoaiPhieuXuat(LoaiPhieuXuatDTO loaiPhieuXuatDTO)
+        {
+            return LoaiPhieuXuatDAO.UpdateLoaiPhieuXuat(loaiPhieuXuatDTO);
+        }
+
+        public static bool DeleteLoaiPhieuXuat(LoaiPhieuXuatDTO loaiPhieuXuatDTO)
+        {
+            return LoaiPhieuXuatDAO.DeleteLoaiPhieuXuat(loaiPhieuXuatDTO);
+        }
+
+        public static List<LoaiPhieuXuatDTO> SelectLoaiPhieuXuatAll()
+        {
+            return LoaiPhieuXuatDAO.SelectLoaiPhieuXuatAll();
+        }
+
+        public static LoaiPhieuXuatDTO SelectLoaiPhieuXuatById(string MaLoaiPhieuXuat)
+        {
+            return LoaiPhieuXuatDAO.SelectLoaiPhieuXuatById(MaLoaiPhieuXuat);
+        }
     }
 }
