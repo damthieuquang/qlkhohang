4ea5cda91ab04452ae0064ff5ed06d92f5455397
phale20202007@gmail.com <phale20202007@gmail.com@3525e459-eec0-bede-41f7-e7cad6b7dfdf>
2013-05-27 08:05:40 +0000

    Tao ChiTietPhieuXuatBUS, LoaiPhieuXuatBUS,LoaiSanPhamBUS

--- Source/QuanLyKhoHang/BUS/LoaiSanPhamBUS.cs
+++ Source/QuanLyKhoHang/BUS/LoaiSanPhamBUS.cs
@@ -9,5 +9,29 @@ namespace BUS
 {
     public class LoaiSanPhamBUS
     {
+        public static bool InsertLoaiSanPham(LoaiSanPhamDTO loaiSanPhamDTO)
+        {
+            return LoaiSanPhamDAO.InsertLoaiSanPham(loaiSanPhamDTO);
+        }
+
+        public static bool UpdateLoaiSanPham(LoaiSanPhamDTO loaiSanPhamDTO)
+        {
+            return LoaiSanPhamDAO.UpdateLoaiSanPham(loaiSanPhamDTO);
+        }
+
+        public static bool DeleteLoaiSanPham(LoaiSanPhamDTO loaiSanPhamDTO)
+        {
+            return LoaiSanPhamDAO.DeleteLoaiSanPham(loaiSanPhamDTO);
+        }
+
+        public static List<LoaiSanPhamDTO> SelectLoaiSanPhamAll()
+        {
+            return LoaiSanPhamDAO.SelectLoaiSanPhamAll();
+        }
+
+        public static LoaiSanPhamDTO SelectLoaiSanPhamById(string MaLoaiSanPham)
+        {
+            return LoaiSanPhamDAO.SelectLoaiSanPhamById(MaLoaiSanPham);
+        }
     }
 }
