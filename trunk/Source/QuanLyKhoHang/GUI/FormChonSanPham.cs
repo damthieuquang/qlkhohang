36d918708388761abd98cc5c1ddbc363b8024289
damthieuquang@gmail.com <damthieuquang@gmail.com@3525e459-eec0-bede-41f7-e7cad6b7dfdf>
2013-05-31 01:03:19 +0000

    Thiết kế thêm màn hình chọn sản phẩm cho người dùng (đã hoàn tất)

---
+++ trunk/Source/QuanLyKhoHang/GUI/FormChonSanPham.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using DTO;
+using BUS;
+
+namespace GUI
+{
+    public partial class FormChonSanPham : Form
+    {
+        public FormChonSanPham()
+        {
+            InitializeComponent();
+        }
+
+        public List<SanPhamDTO> listsanPhamDTO_ChonSanPham = new List<SanPhamDTO>();
+
+        private void FormChonSanPham_Load(object sender, EventArgs e)
+        {
+            List<SanPhamDTO> sanPhamDTO = new List<SanPhamDTO>();
+            sanPhamDTO = SanPhamBUS.SelectSanPhamAll();
+
+            for (int i = 0; i < sanPhamDTO.Count; i++)
+            {
+                dataGridView_ChoSanPham.Rows.Add(i + 1, sanPhamDTO[i].MaSanPham, sanPhamDTO[i].TenSanPham);
+            }
+
+        }
+
+        private void dataGridView_ChoSanPham_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView_ChoSanPham.CurrentRow.Cells[e.ColumnIndex].Selected)
+            {                
+                SanPhamDTO sanpham = new SanPhamDTO();
+                sanpham.MaSanPham = dataGridView_ChoSanPham.CurrentRow.Cells["clMaSanPham"].Value.ToString();
+                listsanPhamDTO_ChonSanPham.Add(sanpham);
+            }
+        }
+
+        private void btn_Xong_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btn_Huy_Click(object sender, EventArgs e)
+        {
+            listsanPhamDTO_ChonSanPham.Clear();
+            Close();
+        }
+    }
+}
