584a6c5fb5b4b1ee87cbe09dcccfc9ffaadc80de
damthieuquang <damthieuquang@3525e459-eec0-bede-41f7-e7cad6b7dfdf>
2014-03-08 04:46:39 +0000

    Sửa kết nối bằng file ConnectText.txt

--- trunk/PTTK/QLKhoHang/GUI/DAO/DataProvider.cs
+++ trunk/PTTK/QLKhoHang/GUI/DAO/DataProvider.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Data.SqlClient;
 using System.Data;
+using System.IO;
 
 namespace DAO
 {
@@ -13,10 +14,52 @@ namespace DAO
 
         public static string ConnectionString
         {
-            get { return @"Data Source=THIEUQUANG\SQLEXPRESS;Initial Catalog=QLKhoHang;Integrated Security=True"; }
+            get { return MakeString(); }
             set { _connectionString = value; }
         }
+        //@"Data Source=THIEUQUANG\SQLEXPRESS;Initial Catalog=QLKhoHang;Integrated Security=True"
+        private static List<string> ReadTextConnection()
+        {
+            List<string> str = new List<string>();
+            try
+            {
+                string currentpath = Directory.GetCurrentDirectory().ToString();
+                string dir = Directory.GetParent(currentpath).ToString();
+                for (int i = 0; i < 2; i++)                
+                    dir = Directory.GetParent(dir).ToString();
+                                
+                dir+=@"\ConnectText.txt";
+                StreamReader reader = new StreamReader(dir);
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    str.Add(line);
+                }
+                reader.Close();
+            }
+            catch (Exception)
+            {
 
+                throw;
+            }                        
+            return str;
+        }
+
+        private static string MakeString()
+        {
+            string strConn;
+            List<string> lst = new List<string>();
+            lst = ReadTextConnection();
+            if(lst.Count<=2)
+            {
+                strConn = string.Format("Data Source={0};Initial Catalog={1};Integrated Security=True", lst[0], lst[1]);
+            }
+            else
+            {
+                strConn = string.Format("Data Source={0};Initial Catalog={1};Integrated Security=SSPI;User ID={2};Password={3};",lst[0],lst[1],lst[2],lst[3]);
+            }
+            return strConn;
+        }
         //ExecuteNoneQuery
         public static bool ExecuteNoneQuery(String spName, List<SqlParameter> sqlParams)
         {
